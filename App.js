42ffe282a30620e78349e8a55bf118880631c429
Aarush Ambar <lmo588003@gmail.com>
2025-07-07 13:33:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Optional: only if you have global styles
+import ImageGenerator from './components/ImageGenerator'; // Make sure the path and file name are correct
+
+function App() {
+  return (
+    <div className="App">
+      <ImageGenerator />
+    </div>
+  );
+}
+
+export default App;
